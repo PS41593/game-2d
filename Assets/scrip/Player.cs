3f964082c4ded991d7d8ca2ac8421c5890bdb874
PS41593 <163637694+PS41593@users.noreply.github.com>
2024-05-11 13:40:24 +0700

    them code

--- Assets/scrip/Player.cs
+++ Assets/scrip/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,14 +8,27 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]private float movespeed;
     [SerializeField]private float jumpspeed;
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    private void Move()
+    {
+        var Move = Input.GetAxis("Horizontal");
+        transform.localPosition += new Vector3(Move, 0, 0) * movespeed * Time.deltaTime; 
+    }
+    public void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+        }
     }
 }
