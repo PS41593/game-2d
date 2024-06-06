8eff98f66dbb6582fec8237cd999383d0f3b56bb
PS41593 <163637694+PS41593@users.noreply.github.com>
2024-06-07 02:08:55 +0700

    d

--- Assets/scrip/exit ponit.cs
+++ Assets/scrip/exit ponit.cs
@@ -24,7 +24,29 @@ public class exitponit : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            informationcanvas.SetActive(false);
             
+           var Score = FindObjectOfType<GameController>().getScore();
+            // lưu Thành tích Của người chơi 
+            var gamedata = new GameData()
+            {
+                score = Score,
+                timeplayed = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+            };
+            played.plays.Add(gamedata);
+            storageHelper.SaveData();
+            played = storageHelper.played;
+            Debug.Log("Count: " + played.plays.Count);
+            // tải đữ liệu trong file hiển thị lên bảng thành tích
+            played.plays.Sort((x,y) => y.score.CompareTo(x.score));
+            for (int i = 0; i < played.plays.Count; i++)
+            {
+                var rowIntace = Instantiate(row, row.transform.parent);
+                rowIntace.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = (i + 1).ToString();
+                rowIntace.transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = played.plays[i].score.ToString();
+                rowIntace.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>().text = played.plays[i].timeplayed.ToString();
+
+            }
 
 
             finishCanvas.SetActive(true);
